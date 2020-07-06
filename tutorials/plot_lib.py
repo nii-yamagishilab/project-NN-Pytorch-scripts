95b10d4f1efc9abdcfb49856c3914294ee47a0dc
TonyWangX <wangxin@nii.ac.jp>
2020-07-06 23:38:48 +0900

    Update tutorial jupyter book

--- tutorials/plot_lib.py
+++ tutorials/plot_lib.py
@@ -5,12 +5,13 @@ import matplotlib.pyplot as plt
 matplotlib.rcParams['figure.figsize'] = (10, 5)
 
 
-def plot_signal(data, fig, axis, xlabel, ylabel):
+def plot_signal(data, fig, axis, xlabel='', ylabel='', title=''):
     """
     """
     axis.plot(data)
     axis.set_xlabel(xlabel)
     axis.set_ylabel(ylabel)
+    axis.set_title(title)
     axis.set_xlim(0, data.shape[0]-1)
     return fig, axis
 
@@ -21,4 +22,10 @@ def plot_signal_stem(data, fig, axis, xlabel, ylabel):
     axis.set_xlabel(xlabel)
     axis.set_ylabel(ylabel)
     axis.set_xlim(0, data.shape[0]-1)
+    return fig, axis
+
+def plot_imshow(data, fig, axis, xlabel, ylabel):
+    axis.imshow(data, aspect='auto', origin='lower', cmap='RdBu')
+    axis.set_xlabel(xlabel)
+    axis.set_ylabel(ylabel)
     return fig, axis
