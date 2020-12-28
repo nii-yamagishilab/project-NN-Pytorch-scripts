14e9fea557c599e98a3cd37d119aa4417733dd32
TonyWangX <wangxin@nii.ac.jp>
2020-12-28 17:01:39 +0900

    LFB front-end (v1 test)

--- sandbox/util_frontend.py
+++ sandbox/util_frontend.py
@@ -1,10 +1,22 @@
+#!/usr/bin/env python
 """
 util_frontend.py
 
 Utilities for frontend feature extraction
 
-LFCC: asvspoof.org baseline matlab code
+It includes:
+ LFCC: based on asvspoof.org baseline matlab code
+ LFB: Linear filterbank feature
+      Chen, T., Kumar, A., Nagarsheth, P., Sivaraman, G. & Khoury, E. 
+      Generalization of Audio Deepfake Detection. in Proc. Odyssey 132-137 
+      (2020). doi:10.21437/Odyssey.2020-19 
 
+      According to the author's email:
+      LFB = np.log(FilterBank(Amplitude(STFT(x))))
+      There is no DCT. But it does have logarithm.
+
+      Implemented based on LFCC API
+ 
 """
 
 from __future__ import absolute_import
@@ -21,7 +33,7 @@ import core_scripts.data_io.conf as nii_conf
 
 __author__ = "Xin Wang"
 __email__ = "wangxin@nii.ac.jp"
-__copyright__ = "Copyright 2020, Xin Wang"
+__copyright__ = "Copyright 2021, Xin Wang"
 
 ##################
 ## other utilities
@@ -61,7 +73,7 @@ def delta(x):
     ------
     output (batch, Length, dim)
     
-    Delta is calculated along Length
+    Delta is calculated along Length dimension
     """
     length = x.shape[1]
     output = torch.zeros_like(x)
@@ -77,11 +89,25 @@ def delta(x):
 class LFCC(torch_nn.Module):
     """ Based on asvspoof.org baseline Matlab code.
     Difference: with_energy is added to set the first dimension as energy
-    
     """
     def __init__(self, fl, fs, fn, sr, filter_num, 
                  with_energy=False, with_emphasis=True,
-                 with_delta=True):
+                 with_delta=True, flag_for_LFB=False):
+        """ Initialize LFCC
+        
+        Para:
+        -----
+          fl: int, frame length, (number of waveform points)
+          fs: int, frame shift, (number of waveform points)
+          fn: int, FFT points
+          sr: int, sampling rate (Hz)
+          filter_num: int, number of filters in filter-bank
+          with_energy: bool, (default False), whether replace 1st dim to energy
+          with_emphasis: bool, (default True), whether pre-emphaze input wav
+          with_delta: bool, (default True), whether use delta and delta-delta
+        
+          for_LFB: bool (default False), reserved for LFB feature
+        """
         super(LFCC, self).__init__()
         self.fl = fl
         self.fs = fs
@@ -89,6 +115,7 @@ class LFCC(torch_nn.Module):
         self.sr = sr
         self.filter_num = filter_num
         
+        # build the triangle filter bank
         f = (sr / 2) * torch.linspace(0, 1, fn//2+1)
         filter_bands = torch.linspace(min(f), max(f), filter_num+2)
         
@@ -99,10 +126,15 @@ class LFCC(torch_nn.Module):
                     filter_bands[idx+1], 
                     filter_bands[idx+2]])
         self.lfcc_fb = torch_nn.Parameter(filter_bank, requires_grad=False)
+
+        # DCT as a linear transformation layer
         self.l_dct = nii_dsp.LinearDCT(filter_num, 'dct', norm='ortho')
+
+        # opts
         self.with_energy = with_energy
         self.with_emphasis = with_emphasis
         self.with_delta = with_delta
+        self.flag_for_LFB = flag_for_LFB
         return
     
     def forward(self, x):
@@ -131,8 +163,8 @@ class LFCC(torch_nn.Module):
         fb_feature = torch.log10(torch.matmul(sp_amp, self.lfcc_fb) + 
                                  torch.finfo(torch.float32).eps)
         
-        # DCT
-        lfcc = self.l_dct(fb_feature)
+        # DCT (if necessary, remove DCT)
+        lfcc = self.l_dct(fb_feature) if not self.flag_for_LFB else fb_feature
         
         # Add energy 
         if self.with_energy:
@@ -151,6 +183,51 @@ class LFCC(torch_nn.Module):
 
         # done
         return lfcc_output
+
+
+#################
+## LFB front-end
+#################
+
+class LFB(LFCC):
+    """ Linear filterbank feature
+      Chen, T., Kumar, A., Nagarsheth, P., Sivaraman, G. & Khoury, E. 
+      Generalization of Audio Deepfake Detection. in Proc. Odyssey 132-137 
+      (2020). doi:10.21437/Odyssey.2020-19 
+       
+    """
+    def __init__(self, fl, fs, fn, sr, filter_num, 
+                 with_energy=False, with_emphasis=True,
+                 with_delta=False):
+        """ Initialize LFB
+        
+        Para:
+        -----
+          fl: int, frame length, (number of waveform points)
+          fs: int, frame shift, (number of waveform points)
+          fn: int, FFT points
+          sr: int, sampling rate (Hz)
+          filter_num: int, number of filters in filter-bank
+          with_energy: bool, (default False), whether replace 1st dim to energy
+          with_emphasis: bool, (default True), whether pre-emphaze input wav
+          with_delta: bool, (default True), whether use delta and delta-delta
+        """
+        super(LFB, self).__init__(fl, fs, fn, sr, filter_num, with_energy,
+                                  with_emphasis, with_delta, flag_for_LFB=True)
+        return
+    
+    def forward(self, x):
+        """
+        input:
+        ------
+         x: tensor(batch, length), where length is waveform length
+        
+        output:
+        -------
+         lfb_output: tensor(batch, frame_num, dim_num)
+        """
+        return super(LFB, self).forward(x)
+
 
 if __name__ == "__main__":
     print("Definition of front-end for Anti-spoofing")
