58ddf771599bbf9c3a33f91b2ae1a669885208fd
TonyWangX <wangxin@nii.ac.jp>
2021-02-02 21:24:34 +0900

    self-attention-pooling with multi-head

--- sandbox/block_nn.py
+++ sandbox/block_nn.py
@@ -709,24 +709,30 @@ class MaxFeatureMap2D(torch_nn.Module):
         return m
 
 
-
 class SelfWeightedPooling(torch_nn.Module):
     """ SelfWeightedPooling module
     Inspired by
     https://github.com/joaomonteirof/e2e_antispoofing/blob/master/model.py
     To avoid confusion, I will call it self weighted pooling
-    """
     
-    def __init__(self, feature_dim, mean_only=False):
-        """ SelfWeightedPooling(feature_dim, mean_only)
+    l_selfpool = SelfWeightedPooling(5, 1, False)
+    with torch.no_grad():
+        input_data = torch.rand([3, 10, 5])
+        output_data = l_selfpool(input_data)
+    """
+    def __init__(self, feature_dim, num_head=1, mean_only=False):
+        """ SelfWeightedPooling(feature_dim, num_head=1, mean_only=False)
         Attention-based pooling
         
         input (batchsize, length, feature_dim) ->
-        output (batchsize, feature_dim) or (batchsize, feature_dim*2)
+        output 
+           (batchsize, feature_dim * num_head), when mean_only=True
+           (batchsize, feature_dim * num_head * 2), when mean_only=False
         
         args
         ----
           feature_dim: dimension of input tensor
+          num_head: number of heads of attention
           mean_only: whether compute mean or mean with std
                      False: output will be (batchsize, feature_dim*2)
                      True: output will be (batchsize, feature_dim)
@@ -736,11 +742,75 @@ class SelfWeightedPooling(torch_nn.Module):
         self.feature_dim = feature_dim
         self.mean_only = mean_only
         self.noise_std = 1e-5
+        self.num_head = num_head
 
+        # transformation matrix (num_head, feature_dim)
         self.mm_weights = torch_nn.Parameter(
-            torch.Tensor(1, feature_dim), requires_grad=True)
+            torch.Tensor(num_head, feature_dim), requires_grad=True)
         torch_init.kaiming_uniform_(self.mm_weights)
+        return
+    
+    def _forward(self, inputs):
+        """ output, attention = forward(inputs)
+        inputs
+        ------
+          inputs: tensor, shape (batchsize, length, feature_dim)
+        
+        output
+        ------
+          output: tensor
+           (batchsize, feature_dim * num_head), when mean_only=True
+           (batchsize, feature_dim * num_head * 2), when mean_only=False
+          attention: tensor, shape (batchsize, length, num_head)
+        """        
+        # batch size
+        batch_size = inputs.size(0)
+        # feature dimension
+        feat_dim = inputs.size(2)
+        
+        # input is (batch, legth, feature_dim)
+        # change mm_weights to (batchsize, feature_dim, num_head)
+        # weights will be in shape (batchsize, length, num_head)
+        weights = torch.bmm(inputs, 
+                            self.mm_weights.permute(1, 0).contiguous()\
+                            .unsqueeze(0).repeat(batch_size, 1, 1))
+        
+        # attention (batchsize, length, num_head)
+        attentions = torch_nn_func.softmax(torch.tanh(weights),dim=1)        
+        
+        # apply attention weight to input vectors
+        if self.num_head == 1:
+            # We can use the mode below to compute self.num_head too
+            # But there is numerical difference.
+            #  original implementation in github
+            
+            # elmentwise multiplication
+            # weighted input vector: (batchsize, length, feature_dim)
+            weighted = torch.mul(inputs, attentions.expand_as(inputs))
+        else:
+            # weights_mat = (batch * length, feat_dim, num_head)
+            weighted = torch.bmm(
+                inputs.view(-1, feat_dim, 1), 
+                attentions.view(-1, 1, self.num_head))
+            
+            # weights_mat = (batch, length, feat_dim * num_head)
+            weighted = weighted.view(batch_size, -1, feat_dim * self.num_head)
+            
+        # pooling
+        if self.mean_only:
+            # only output the mean vector
+            representations = weighted.sum(1)
+        else:
+            # output the mean and std vector
+            noise = self.noise_std * torch.randn(
+                weighted.size(), dtype=weighted.dtype, device=weighted.device)
 
+            avg_repr, std_repr = weighted.sum(1), (weighted+noise).std(1)
+            # concatenate mean and std
+            representations = torch.cat((avg_repr,std_repr),1)
+        # done
+        return representations, attentions
+    
     def forward(self, inputs):
         """ output = forward(inputs)
         inputs
@@ -749,84 +819,15 @@ class SelfWeightedPooling(torch_nn.Module):
         
         output
         ------
-          output: tensor, shape (batchsize, feature_dim*2) if mean_only is False
-                  tensor, shape (batchsize, feature_dim) if mean_only is True
+          output: tensor
+           (batchsize, feature_dim * num_head), when mean_only=True
+           (batchsize, feature_dim * num_head * 2), when mean_only=False
         """
-        
-        # batch matrix multiplication
-        batch_size = inputs.size(0)
-        # change mm_weights to (batchsize, feature_dim, 1)
-        # weights in shape (batchsize, length, 1)
-        weights = torch.bmm(
-            inputs, 
-            self.mm_weights.permute(1, 0).contiguous()\
-            .unsqueeze(0).repeat(batch_size, 1, 1))
-        
-        # attention (batchsize, length, 1)
-        attentions = torch_nn_func.softmax(torch.tanh(weights),dim=1)
+        output, _ = self._forward(inputs)
+        return output
 
-        # pooling
-        # weighted (batchsize, length, feature_dim)
-        weighted = torch.mul(inputs, attentions.expand_as(inputs))
-        
-        if self.mean_only:
-            # only output the mean vector
-            return weighted.sum(1)
-        else:
-            # output the mean and std vector
-            noise = self.noise_std * torch.randn(
-                weighted.size(), dtype=weighted.dtype, device=weighted.device)
-
-            avg_repr, std_repr = weighted.sum(1), (weighted+noise).std(1)
-
-            # concatenate mean and std
-            representations = torch.cat((avg_repr,std_repr),1)
-            return representations
-        
     def debug(self, inputs):
-        """ similar to forward(inputs), but return intermediate results
-        for debugging: output_tensor, attentions = debug(inputs)
-        
-        inputs
-        ------
-          input: tensor of shape (batchsize, length, feature_dim)
-        
-        output
-        ------
-          output: tensor, shape (batchsize, feature_dim*2) or 
-                  (batchsize, feature_dim)
-          attention: tensor, shape (batchsize, length, 1)
-        """
-        
-        # batch matrix multiplication
-        batch_size = inputs.size(0)
-        # change mm_weights to (batchsize, feature_dim, 1)
-        # weights in shape (batchsize, length, 1)
-        weights = torch.bmm(
-            inputs, 
-            self.mm_weights.permute(1, 0).contiguous()\
-            .unsqueeze(0).repeat(batch_size, 1, 1))
-        
-        # attention (batchsize, length, 1)
-        attentions = torch_nn_func.softmax(torch.tanh(weights),dim=1)
-
-        # pooling
-        # weighted (batchsize, length, feature_dim)
-        weighted = torch.mul(inputs, attentions.expand_as(inputs))
-        
-        if self.mean_only:
-            # only output the mean vector
-            output_tensor = weighted.sum(1)
-        else:
-            # output the mean and std vector
-            noise = self.noise_std * torch.randn(
-                weighted.size(), dtype=weighted.dtype, device=weighted.device)
-
-            avg_repr, std_repr = weighted.sum(1), (weighted+noise).std(1)
-
-            # concatenate mean and std
-            output_tensor = torch.cat((avg_repr,std_repr),1)
-        return output_tensor, attentions
+        return self._forward(inputs)
 
 
 class Conv1dForARModel(Conv1dKeepLength):
