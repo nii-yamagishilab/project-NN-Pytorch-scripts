1eff5fa6e9f0c0a44f5280dffc8aae08d8f31ed1
TonyWangX <wangxin@nii.ac.jp>
2021-07-07 00:37:59 +0900

    Handle invalid reflection-coef in levison-durbin

--- core_scripts/data_io/dsp_tools.py
+++ core_scripts/data_io/dsp_tools.py
@@ -157,16 +157,21 @@ class Melspec(object):
 class LPClite(object):
     """ A lite LPC analyzer & synthesizr
     Note that this is based on numpy, not Pytorch
-    It can be used for pre-processing in dataloader
+    It can be used for pre-processing when loading data, or use
+    it as data transformation function
+    (see message at top)
     
     Example:
+        # load waveform
         sr, wav = wav_tools.waveReadAsFloat(wav_path)
         m_lpc = LPClite(320, 80)
         # LPC analysis 
-        lpc_coef, _, rc, gain, err = m_lpc.analysis(wav)
+        lpc_coef, _, rc, gain, err, err_overlapped = m_lpc.analysis(wav)
         # LPC synthesis
         wav_re = m_lpc.synthesis(lpc_coef, err, gain)
-        
+        # rc to LPC 
+        lpc_coef_tmp = m_lpc._rc2lpc(lpc_coef)
+        np.std(lpc_coef_tmp - lpc_coef)
     """
     def __init__(self, fl=320, fs=80, order=30, window='blackman'):
         """LPClite(fl=320, fs=80, order=30, window='blackman')
@@ -174,8 +179,13 @@ class LPClite(object):
         ----
           fl: int, frame length
           fs: int, frame shift
-          order: int, order of LPC, [1, a_2, a_3, ..., a_order]
+          order: int, order of LPC, [1, a_1, a_2, ..., a_order-1]
           window: str, 'blackman' or 'hanning'
+        
+        Note that LPC model is defined as:
+                            Gain 
+        ---------------------------------------------
+        a_0 + a_1 z^-1 + ... + a_order-1 z^-(order-1)
         """
         self.fl = fl
         self.fs = fs
@@ -189,33 +199,41 @@ class LPClite(object):
         return
     
     def analysis(self, wav):
-        """lpc_coef, ld_err, gamma_array, gain, framed_err, err = analysis(wav)
+        """lpc_coef, ld_err, gamma, gain, framed_err, err_signal = analysis(wav)
         
         LPC analysis on each frame
         
         input
         -----
-          wav: np.array, (length, )
+          wav: np.array, (length, 1)
           
         output
         ------
-          lpc_coef: np.array, LPC coeff, (frame_num, lpc_order)
-          ld_err: np.array, LD analysis error, (frame_num, lpc_order)
-          gamma: np.array, reflection coefficients, (frame_num, lpc_order)
-          gain: np.array, gain, (frame_num, 1)
-          framed_err: np.array, LPC error, (frame_num, frame_length)
-          eer_signal: np.array, (length, )
+          lpc_coef:   np.array, LPC coeff, (frame_num, lpc_order)
+          ld_err:     np.array, LD analysis error, (frame_num, lpc_order)
+          gamma:      np.array, reflection coefficients, (frame_num,lpc_order-1)
+          gain:       np.array, gain, (frame_num, 1)
+          framed_err: np.array, LPC error per frame, (frame_num, frame_length)
+          eer_signal: np.array, overlap-added excitation (length, 1)
+
+        Note that framed_err is the excitation signal from LPC analysis on each
+        frame. eer_signal is the overlap-added excitation signal.
         """
         # framing & windowing
-        frame_wined = self._windowing(self._framing(wav))
+        frame_wined = self._windowing(self._framing(wav[:, 0]))
+
         # auto-correlation
         auto = self._auto_correlation(frame_wined)
+
         # LD analysis
         lpc_coef, lpc_err, gamma_array, gain = self._levison_durbin(auto)
-        # get LPC error
+
+        # get LPC excitation signals in each frame
         framed_err = self._lpc_analysis_core(lpc_coef, frame_wined, gain)
-        # overlap add the error signal
+
+        # overlap-add for excitation signal
         err_signal = self._overlapadd(framed_err)
+        
         return lpc_coef, lpc_err, gamma_array, gain, framed_err, err_signal
     
     def synthesis(self, lpc_coef, framed_err, gain):
@@ -225,18 +243,33 @@ class LPClite(object):
         
         input
         -----
-          lpc_coef: np.array, LPC coeff, (frame_num, lpc_order)
-          gain: np.array, gain, (frame_num, 1)
-          framed_err: np.array, LPC error, (frame_num, frame_length)
-
+          lpc_coef:   np.array, LPC coeff, (frame_num, lpc_order)
+          framed_err: np.array, LPC excitations, (frame_num, frame_length)
+          gain:       np.array, LPC gain, (frame_num, 1)
+        
         output
         ------
-          wav: np.array, (length, )
+          wav: np.array, (length, 1)
+        
+        This function does LPC synthesis in each frame and create
+        the output waveform by overlap-adding
         """
         framed_x = self._lpc_synthesis_core(lpc_coef, framed_err, gain)
         return self._overlapadd(framed_x)
     
     def _framing(self, wav):
+        """F = _framed(wav)
+        
+        Framing the signal
+        
+        input
+        -----
+          wav: np.array, (length)
+
+        output
+        ------
+          F: np.array, (frame_num, frame_length)
+        """
         frame_num = (wav.shape[0] - self.fl) // self.fs + 1
         F = np.zeros([frame_num, self.fl], dtype=wav.dtype)
         for frame_idx in np.arange(frame_num):
@@ -244,13 +277,31 @@ class LPClite(object):
         return F
     
     def _windowing(self, framed_x):
+        """windowing
+        """
         return framed_x * self.win
     
     
     def _overlapadd(self, framed_x):
+        """wav = _overlapadd(framed_x)
+        
+        Do overlap-add on framed (and windowed) signal
+        
+        input
+        -----
+          framed_x: np.array, (frame_num, frame_length)
+          
+        output
+        ------
+          wav: np.array, (length, 1)
+        
+        length = (frame_num - 1) * frame_shift + frame_length
+        """
+        # waveform length
         wavlen = (framed_x.shape[0] - 1) * self.fs + self.fl
         wavbuf = np.zeros([wavlen])
         
+        # buf to save overlapped windows (to normalize the signal amplitude)
         protobuf = np.zeros([wavlen])
         win_prototype = self._windowing(self._framing(np.ones_like(protobuf)))
         
@@ -259,9 +310,11 @@ class LPClite(object):
             frame_s = idx * self.fs
             wavbuf[frame_s : frame_s + self.fl] += framed_x[idx]
             protobuf[frame_s : frame_s + self.fl] += win_prototype[idx]
+        
+        # remove the impact of overlapped windows
         #protobuf[protobuf<1e-05] = 1.0
         wavbuf = wavbuf / protobuf.mean()
-        return wavbuf
+        return np.expand_dims(wavbuf, axis=1)
     
     def _lpc_analysis_core(self, lpc_coef, framed_x, gain):
         """framed_err = _lpc_analysis_core(lpc_coef, framed_x, gain)
@@ -273,13 +326,13 @@ class LPClite(object):
         -----
           lpc_coef: np.array, (frame_num, order)
           framed_x: np.array, (frame_num, frame_length)
-          gain: np.array, (frame_num, )
+          gain: np.array,     (frame_num, 1)
           
         output
         ------
           framed_err: np.array, (frame_num, frame_length)
         
-        Note that lpc_coef[n, :] = (1, a_2, a_3, ..., a_order) for n-th frame
+        Note that lpc_coef[n, :] = (1, a_1, a_2, ..., a_order-1) for n-th frame
         framed_x[n, :] = (x[0], x[1], ..., x[frame_len]) for n-th frame
         """
         # 
@@ -289,76 +342,121 @@ class LPClite(object):
         # including the 1st 1.0
         order = lpc_coef.shape[1]
         
-        # pad with zero  (0, 0, ..., 0, x[0], x[1], ..., x[frame_len])
+        # pad zero, every frame has [0, ..., 0, x[0], x[1], ..., x[frame_len]]
         tmp_framed = np.concatenate(
             [np.zeros([frame_num, order]), framed_x], axis=1)
         
-        # reverse to (x[frame_len], ... x[1], x[0]. 0, 0)
+        # flip to (x[frame_len], ... x[1], x[0], 0, ..., 0)
         tmp_framed = tmp_framed[:, ::-1]
-        
+
+        # LPC excitation buffer
         framed_err = np.zeros_like(framed_x)
-        for idx in range(self.order):
-            # (idx+1)-order cofficients for each frame, (frame_num, 1)
-            tmp_coef = lpc_coef[:, idx:idx+1]
+        
+        # e[n] = \sum_k=0 a[k] x[n-k] 
+        # do this for all frames and n simultaneously
+        for k in range(self.order):
+            # a[k]
+            tmp_coef = lpc_coef[:, k:k+1]
             
-            # x[n-idx] * coef[idx], do it for all time steps
+            # For each frame
+            # RHS = [x[n-k], x[n-k-1], ..., ] * a[k]
+            # 
+            # By doing this for k in [0, order-1]
+            # LHS = [e[n],   e[n-1],   ...]
+            #       [x[n-0], x[n-0-1], ..., ] * a[0]
+            #     + [x[n-1], x[n-1-1], ..., ] * a[1]
+            #     + [x[n-2], x[n-2-1], ..., ] * a[2]
+            #     + ...
+            # We get the excitation for one frame
+            # This process is conducted for all frames at the same time
             framed_err += tmp_framed[:, 0:frame_len] * tmp_coef
             
-            # (x[frame_len-1], ... x[1], x[0]. 0, 0, x[frame_en])
+            # roll to [x[n-k-1], x[n-k-2], ..., ]
             tmp_framed = np.roll(tmp_framed, -1, axis=1)
 
         # revese to (e[0], e[1], ..., e[frame_len])
-        return framed_err[:, ::-1] / np.expand_dims(gain, axis=1)
+        return framed_err[:, ::-1] / gain
     
     def _lpc_synthesis_core(self, lpc_coef, framed_err, gain):
         """framed_x = _lpc_synthesis_core(lpc_coef, framed_err, gain)
+        
         AR filtering: x[n] = gain * e[n] - \sum_k=0 a_k x[n-k]
         LPC synthesis on frame
         
         input
         -----
-          lpc_coef: np.array, (frame_num, order)
+          lpc_coef:   np.array, (frame_num, order)
           framed_err: np.array, (frame_num, frame_length)
-          gain: np.array, (frame_num, )
+          gain:       np.array, (frame_num, 1)
           
         output
         ------
-          framed_x: np.array, (frame_num, frame_length)
+          framed_x:   np.array, (frame_num, frame_length)
         
-        Note that lpc_coef[n, :] = (1, a_2, a_3, ..., a_order) for n-th frame
-        framed_x[n, :] = (x[0], x[1], ..., x[frame_len]) for n-th frame
+        Note that 
+        lpc_coef[n, :] = (1, a_1, a_2, ..., a_order-1),  for n-th frame
+        framed_x[n, :] = (x[0], x[1], ..., x[frame_len]), for n-th frame
         """
         frame_num = framed_err.shape[0]
         frame_len = framed_err.shape[1]
         order = lpc_coef.shape[1]
         
+        # pad zero 
+        # the buffer looks like 
+        #  [[0, 0, 0, 0, 0, ... x[0], x[1], x[frame_length -1]],  -> 1st frame
+        #   [0, 0, 0, 0, 0, ... x[0], x[1], x[frame_length -1]],  -> 2nd frame
+        #   ...] 
         framed_x = np.concatenate(
             [np.zeros([frame_num, order-1]), np.zeros_like(framed_err)], axis=1)
         
-        # it is convenient to do this so that [a_order, ..., a_1, 1]
+        # flip the cofficients of each frame as [a_order-1, ..., a_1, 1]
         lpc_coef_tmp = lpc_coef[:, ::-1]
         
+        # synthesis (all frames are down at the same time)
         for idx in range(frame_len):
-            framed_x[:, idx+order-1] = framed_err[:, idx] * gain
-            # [x[n-order], ..., x[n-1], err] * [a_order, a_1, 1]
-            framed_x[:, idx+order-1] += np.sum(
-                framed_x[:, idx:idx+order-1] * -lpc_coef_tmp[:, :-1], axis=1)
+            # idx+order-1 so that it points to the shifted time idx
+            #                                    idx+order-1
+            # [0, 0, 0, 0, 0, ... x[0], x[1], ... x[idx], ]
+            # gain * e[n]
+            framed_x[:, idx+order-1] = framed_err[:, idx] * gain[:, 0]
+            
+            # [x[idx-1-order-1], ..., x[idx-1]] * [a_order, a_1]
+            pred = np.sum(framed_x[:, idx:idx+order-1] * lpc_coef_tmp[:, :-1], 
+                          axis=1)
+            
+            # gain * e[n] - [x[idx-1-order-1], ..., x[idx-1]] * [a_order, a_1]
+            framed_x[:, idx+order-1] = framed_x[:, idx+order-1] - pred
+            
+        # [0, 0, 0, 0, 0, ... x[0], x[1], ... ] -> [x[0], x[1], ...]
         return framed_x[:, order-1:] 
     
     def _auto_correlation(self, framed_x):
-        """
+        """ autocorr = _auto_correlation(framed_x)
+
+        input
+        -----
+          framed_x: np.array, (frame_num, frame_length), frame windowed signal
+        
+        output
+        ------
+          autocorr: np.array, auto-correlation coeff (frame_num, lpc_order)
         """
         # (frame_num, order)
         autocor = np.zeros([framed_x.shape[0], self.order])
         
-        # loop and compute auto-corre
+        # loop and compute auto-corr (for all frames simultaneously)
         for i in np.arange(self.order):
-            autocor[:, i] = 0
-            for idx in np.arange(self.fl):
-                if (idx + i) < self.fl:
-                    autocor[:, i] += framed_x[:, idx] * framed_x[:, idx + i]
-                else:
-                    break
+            autocor[:, i] = np.sum(
+                framed_x[:, 0:self.fl-i] * framed_x[:, i:],
+                axis=1)
+            #print(autocor[0, i])
+            #autocor[:, i] = 0
+            #for idx in np.arange(self.fl):
+            #    if (idx + i) < self.fl:
+            #        autocor[:, i] += framed_x[:, idx] * framed_x[:, idx + i]
+            #    else:
+            #        break
+            #print(autocor[0, i])
         # (frame_num, order)
         return autocor
     
@@ -375,7 +473,7 @@ class LPClite(object):
           lpc_coef: np.array, LPC coefficients, (frame_num, lpc_order)
           lpc_err: np.array, LPC error, (frame_num, lpc_order)
           gamma: np.array, reflection coeff, (frame_num, lpc_order-1)
-          gain: np.array, gain, (frame_num)
+          gain: np.array, gain, (frame_num, 1)
           
         Note that lpc_coef[n] = (1, a_2, ... a_order) for n-th frame
         """
@@ -396,23 +494,31 @@ class LPClite(object):
         lpc_coef[:, 0, 0] = 1.0
 
         for index in np.arange(1, polyOrder):
+            
             lpc_coef[:, 1, index] = 1.0
-            gamma = np.sum(
-                lpc_coef[:, 0, 0:(index)] * autocor[:, 1:(index+1)], axis=1)\
-                / lpc_err[:, index-1]
+            
+            # compute gamma
+            #   step1.
+            gamma = np.sum(lpc_coef[:, 0, 0:(index)] * autocor[:, 1:(index+1)], 
+                           axis=1)
+            #   step2. check validity of lpc_err
+            ill_idx = np.abs(lpc_err[:,index-1]) < 1e-07
+            #      also frames that should have been stopped in previous iter
+            ill_idx = np.bitwise_or(ill_idx, tmp_order < polyOrder)
+            #   step3. make invalid frame gamma=0
+            gamma[ill_idx] = 0
+            gamma[~ill_idx] = gamma[~ill_idx] / lpc_err[~ill_idx,index-1]
             gamma_array[:, index-1] = gamma
+            #   step4. log down the ill frames
+            tmp_order[ill_idx] = index
 
-            tmp_order[np.abs(gamma) > 1.0] = index
-    
             lpc_coef[:, 1, 0] = -1.0 * gamma
             if index > 1:
                 lpc_coef[:, 1, 1:index] = lpc_coef[:, 0, 0:index-1] \
                 + lpc_coef[:, 1, 0:1] * lpc_coef[:, 0, 0:index-1][:, ::-1]
-                #for index2 in np.arange(1, index):
-                #    lpc_coef[:, 1, index2] = lpc_coef[:, 0, index2-1] \
-                #       - gamma * lpc_coef[:, 0, index - 1 - index2]
+
             lpc_err[:, index] = lpc_err[:, index-1] * (1 - gamma * gamma)
-            lpc_coef[:, 0, :] = lpc_coef[:, 1,:]
+            lpc_coef[:, 0, :] = lpc_coef[:, 1, :]
         
         # flip to (1, a_2, ..., a_order)
         lpc_coef = lpc_coef[:, 0, ::-1]
@@ -424,21 +530,43 @@ class LPClite(object):
         # each frame may require a different truncation length
         for idx in range(frame_num):
             lpc_coef_ou[idx, 0:tmp_order[idx]] = lpc_coef[idx, 0:tmp_order[idx]]
+            
+        # get the gain, when tmp_order = polyOrder, tmp_order-2 -> order-1, 
+        #  last element of the lpc_err buffer
         gain = np.sqrt(lpc_err[np.arange(len(tmp_order)), tmp_order-2])
         
+        # if the gain is zero, it means analysis error is zero, 
+        gain[gain < 1e-07] = 1.0
+
         # (frame_num, order)
-        return lpc_coef_ou, lpc_err, gamma_array, gain
+        return lpc_coef_ou, lpc_err, gamma_array, np.expand_dims(gain, axis=1)
     
     def _rc2lpc(self, rc):
-        """from reflection coefficients to LPC coefficients
+        """lpc_coef = _rc2lpc(rc)
+        from reflection coefficients to LPC coefficients
+        forward Levinson recursion
+        
+        input
+        -----
+          rc: np.array, (frame_num, lpc_order - 1)
+        
+        output
+        ------
+          lpc_coef, np.array, (frame_num, lpc_order)
+
+        Note that LPC model is defined as:
+                            Gain 
+        ---------------------------------------------
+        a_0 + a_1 z^-1 + ... + a_order-1 z^-(order-1)
+
+        Thus, the reflection coefficitns [gamma_1, ... gamma_order-1]
         """
         # (frame_num, order)
         frame_num, order = rc.shape 
         polyOrder = order + 1
         
         lpc_coef = np.zeros([frame_num, 2, polyOrder])
-        lpc_coef[:, 0, 0] = 1.0
-        
+        lpc_coef[:, 0, 0] = 1.0        
         for index in np.arange(1, polyOrder):
             lpc_coef[:, 1, index] = 1.0
             gamma = rc[:, index-1]
@@ -446,10 +574,6 @@ class LPClite(object):
             if index > 1:
                 lpc_coef[:, 1, 1:index] = lpc_coef[:, 0, 0:index-1] \
                 + lpc_coef[:, 1, 0:1] * lpc_coef[:, 0, 0:index-1][:, ::-1]
-                #for index2 in np.arange(1, index):
-                #    lpc_coef[:, 1, index2] = lpc_coef[:, 0, index2-1] \
-                #       - gamma * lpc_coef[:, 0, index - 1 - index2]
-            lpc_err[:, index] = lpc_err[:, index-1] * (1 - gamma * gamma)
             lpc_coef[:, 0, :] = lpc_coef[:, 1,:]
         
         lpc_coef = lpc_coef[:, 0, ::-1]
@@ -458,3 +582,29 @@ class LPClite(object):
 
 if __name__ == "__main__":
     print("DSP tools using numpy")
+    
+    # Example for downing LPC analysis
+    sr, data1 = wav_tools.waveReadAsFloat('media/arctic_a0001.wav')
+    m_lpc = LPClite(320, 80)
+
+    # LPC analysis                                                      
+    lpc_coef, _, rc, gain, err, err_overlapped = m_lpc.analysis(
+        np.expand_dims(data1, axis=1))
+
+    # LPC synthesis                                                     
+    wav_re = m_lpc.synthesis(lpc_coef, err, gain)                       
+
+    # excitation with Gain
+    excitation_new = m_lpc._overlapadd(err * gain)
+
+    plot_API.plot_API([wav_re[:, 0], data1, 
+                       err_overlapped[:, 0], 
+                       excitation_new[:, 0]], 
+                      plot_lib.plot_spec, 'v')
+
+    plot_API.plot_API([wav_re[:, 0] - err_overlapped[:, 0]], 
+                      plot_lib.plot_spec, 'single')
+
+    # RC to LPC
+    lpc_coef_tmp = m_lpc._rc2lpc(rc)
+    print(np.std(lpc_coef_tmp - lpc_coef))
